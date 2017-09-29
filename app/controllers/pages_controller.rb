d4ff6cfe5fb6ed714ef4927965faee4eee83fae8
Sam Holst <sawohol@gmail.com>
2017-09-29 16:17:30 -0600

    compiled both merges from Richi

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  access all: [:index], [:user, :admin] => :all
+  access all: [:landing_page], [:user, :admin] => :all
   def index
     if fetch_params
       User.reindex
@@ -9,6 +9,9 @@ class PagesController < ApplicationController
     else
       @users = User.all
     end
+  end
+
+  def landing_page
   end
 
   def user_show

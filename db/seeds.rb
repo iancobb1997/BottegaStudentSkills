6d8cafa324f529cab5612ea9cf56c20c9021ae62
Deven Clingman <dclingy@gmail.com>
2017-09-29 12:01:54 -0600

    implemented data to seeds file and minor change on index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+skill_array = ["ruby", "Rails", "SQL", "HTML", "Angular", "CSS"]
+
+10.times do |u|
+  User.create!(
+    email: "user#{u}@user.com",
+    skills: skill_array.sample,
+    password: "asdfasdf"
+  )
+end
+
+puts "10 users created"
+
+1.times do |a|
+  User.create!(
+    email: "admin#{a}@admin.com",
+    password: "asdfasdf",
+    roles: "admin"
+  )
+end
+
+puts "1 admin created"
